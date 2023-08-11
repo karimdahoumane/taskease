6611dab3ffb1cea24a93ae141df3f16cd3b18f1b
Karim Dahoumane <krmdahoumane@gmail.com>
2023-08-11 14:39:56 +0100

    🎉 Initialize client

---
+++ client/src/components/ArticleCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      <div class="p-8">
+        <a href="#" class="font-medium text-lg text-black">Vue + TailwindCSS</a>
+      </div>
+      <p>
+        Learn how to create visually stunning Vue components using the popular
+        utility-first CSS framework Tailwind.
+      </p>
+    </div>
+  </div>
+</template>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ArticleCard />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ArticleCard from '@/components/ArticleCard.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    ArticleCard,
+  },
+});
+</script>
