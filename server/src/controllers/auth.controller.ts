175211715bbabe2c80c785a272405dcd2aed02d5
Karim Dahoumane <krmdahoumane@gmail.com>
2023-08-15 23:48:09 +0100

    ♻️🗑️ chore(server): Refactor code using asyncCatch method

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,47 +3,38 @@ import { validationResult } from "express-validator";
 import * as AuthService from "../services/auth.service";
 import { IUser } from "../types/user";
 import { BadRequestError } from "../helpers/errors";
+import { asyncCatch } from "../helpers/asyncCatch";
 
-const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const { email, firstName, lastName, password }: IUser = req.body;
+const register = asyncCatch(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const { email, firstName, lastName, password }: IUser = req.body;
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new BadRequestError(errors.array().map((error) => error.msg).join(", "));
-    }
-
-    const registrationResult = await AuthService.registerUser(email, firstName, lastName, password);
-    if (registrationResult.error) {
-      throw new BadRequestError(registrationResult.error);
-    }
-
-    res.status(201).json(registrationResult);
-  } catch (error) {
-    console.error("An error occurred during registration:", error);
-    return next(error);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new BadRequestError(errors.array().map((error) => error.msg).join(", "));
   }
-};
 
-const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new BadRequestError(errors.array().map((error) => error.msg).join(", "));
-    }
-
-    const { email, password }: { email: string, password: string } = req.body;
-    if (!email || !password) {
-      throw new BadRequestError("Email and password are required");
-    }
-
-    const loginResult = await AuthService.loginUser(email, password);
-
-    res.status(200).json(loginResult);
-  } catch (error) {
-    console.error("Login error:", error);
-    return next(error);
+  const registrationResult = await AuthService.registerUser(email, firstName, lastName, password);
+  if (registrationResult.error) {
+    throw new BadRequestError(registrationResult.error);
   }
-};
+
+  res.status(201).json(registrationResult);
+});
+
+const login = asyncCatch(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new BadRequestError(errors.array().map((error) => error.msg).join(", "));
+  }
+
+  const { email, password }: { email: string, password: string } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Email and password are required");
+  }
+
+  const loginResult = await AuthService.loginUser(email, password);
+
+  res.status(200).json(loginResult);
+});
 
 export { register, login };
--- server/src/controllers/todo.controller.ts
+++ server/src/controllers/todo.controller.ts
@@ -5,79 +5,51 @@ import { findUserById } from "../services/user.service";
 import { ITodo } from "../types/todo";
 import { BadRequestError } from "../helpers/errors";
 import { IUser } from "../types/user";
+import { asyncCatch } from "../helpers/asyncCatch";
 
-const getTodos = async (_: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const todos: ITodo[] = await TodoService.findAllTodos();
-    res.status(200).json({ todos });
-  } catch (error) {
-    console.error("Get todos error:", error);
-    return next(error);
+const getTodos = asyncCatch(async (_: Request, res: Response, _next: NextFunction): Promise<void> => {
+  const todos: ITodo[] = await TodoService.findAllTodos();
+  res.status(200).json({ todos });
+});
+
+const getTodo = asyncCatch(async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+  const todo: ITodo = await TodoService.findTodoById(req.params.id);
+  res.status(200).json({ todo });
+});
+
+const createTodo = asyncCatch(async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new BadRequestError(errors.array().map((error) => error.msg).join(", "));
   }
-}
 
-const getTodo = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const todo: ITodo = await TodoService.findTodoById(req.params.id);
-    res.status(200).json({ todo });
-  } catch (error) {
-    console.error("Get todo error:", error);
-    return next(error);
-  }
-}
+  const todoData: Pick<ITodo, "name" | "description" | "done" | "user_id"> = req.body;
+  const user: IUser = await findUserById(todoData.user_id);
+  const newTodo: ITodo = await TodoService.createTodo({
+    name: todoData.name,
+    description: todoData.description,
+    done: todoData.done,
+    user_id: user.id
+  });
 
-const createTodo = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new BadRequestError(errors.array().map((error) => error.msg).join(", "));
-    }
+  res.status(201).json({ todo: newTodo });
+});
 
-    const todoData: Pick<ITodo, "name" | "description" | "done" | "user_id"> = req.body;
-    const user: IUser = await findUserById(todoData.user_id);
-    const newTodo: ITodo = await TodoService.createTodo({
-      name: todoData.name,
-      description: todoData.description,
-      done: todoData.done,
-      user_id: user.id
-    });
+const updateTodo = asyncCatch(async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+  const todoData: ITodo = req.body;
+  await TodoService.updateTodoById(req.params.id, todoData);
+  res.status(200).json({ message: "Todo updated" });
+});
 
-    res.status(201).json({ todo: newTodo });
-  } catch (error) {
-    console.error("Create todo error:", error);
-    return next(error);
-  }
-}
+const deleteTodo = asyncCatch(async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+  await TodoService.deleteTodoById(req.params.id);
+  res.status(200).json({ message: "Todo deleted" });
+});
 
-const updateTodo = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const todoData: ITodo = req.body;
-    await TodoService.updateTodoById(req.params.id, todoData);
-    res.status(200).json({ message: "Todo updated" });
-  } catch (error) {
-    console.error("Update todo error:", error);
-    return next(error);
-  }
-}
 
-const deleteTodo = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    await TodoService.deleteTodoById(req.params.id);
-    res.status(200).json({ message: "Todo deleted" });
-  } catch (error) {
-    console.error("Delete todo error:", error);
-    return next(error);
-  }
-}
-
-const getTodosByUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const todos: ITodo[] = await TodoService.findAllTodosByUser(req.params.id);
-    res.status(200).json({ todos });
-  } catch (error) {
-    console.error("Get todos by user id error:", error);
-    return next(error);
-  }
-}
+const getTodosByUser = asyncCatch(async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+  const todos: ITodo[] = await TodoService.findAllTodosByUser(req.params.id);
+  res.status(200).json({ todos });
+});
 
 export { getTodos, getTodo, createTodo, updateTodo, deleteTodo, getTodosByUser };
