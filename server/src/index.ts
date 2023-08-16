9bc2b5cc300eb75578ac6431b678eae1c9091a51
Karim Dahoumane <krmdahoumane@gmail.com>
2023-08-16 18:08:26 +0100

    🔐 feat(server): Add limit on api calls

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors"
 import todoRoutes from "./routes"
 import dotenv from 'dotenv';
 import exceptionHandler from "./middlewares/error"
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
@@ -11,10 +12,18 @@ const app: Express = express()
 
 const PORT: string | number = process.env.PORT || 3000;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+})
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/api/', todoRoutes)
+app.use(limiter)
+app.use('/api', todoRoutes)
 app.use(exceptionHandler);
 
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@todolist.1vzsfkk.mongodb.net/?retryWrites=true&w=majority`;
